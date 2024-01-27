45079a30e961c63e16626e9e8f2d22ba96e78c16
StarSer <androidemoc@gmail.com>
2024-01-26 17:41:05 -0700

    Agregar archivos de proyecto.

---
+++ leetcodex/Solutions.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodex
+{
+    public class Solutions
+    {
+        public static bool IsPalindrome(int x)
+        {
+            bool isPalindrome = true;
+            string numbers = x.ToString();
+            char[] arraynumbers = new char[numbers.Length];
+            char[] arraynumbersCheck = new char[numbers.Length];
+
+            for (int i = 0, p = numbers.Length - 1; i < numbers.Length; i++, p--)
+            {
+                arraynumbers[i] = Convert.ToChar(numbers[i].ToString());
+                arraynumbersCheck[p] = Convert.ToChar(numbers[i].ToString());
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (arraynumbersCheck[i] != arraynumbers[i])
+                    isPalindrome = false;
+            }
+            return isPalindrome;
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int summation, temp1 = 0, temp2 = 0;
+            for (int i = 0, a = 1; i < nums.Length; a++)
+            {
+                summation = nums[i] + nums[a];
+                if (summation == target)
+                {
+                    temp1 = i; temp2 = a;
+                    break;
+                }
+                if (a == nums.Length - 1)
+                {
+                    i++;
+                    a = i;
+                }
+            }
+            return new int[] { temp1, temp2 };
+        }
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int UnicNums = 1, cont = 0;
+            List<int> list = new List<int>();
+            for (int i = 0, a = 1; a < nums.Length; i++, a++)
+            {
+                if (nums[i] != nums[a])
+                {
+                    UnicNums++;
+                    list.Add(nums[i]);
+                    cont++;
+                }
+                if (a == nums.Length - 1)
+                {
+                    list.Add(nums[a]);
+                }
+            }
+            return UnicNums;
+        }
+
+        public static bool IsHappy(int n)
+        {
+            bool Happy = false;
+            for (int contator = 0; Happy != true && contator < 20; contator++)
+            {
+                double newnum = 0;
+                string num = n.ToString();
+                int[] numArray = new int[num.Length];
+                int i = 0;
+                for (int a = 0; a < num.Length; a++)
+                {
+                    numArray[a] = int.Parse(num[a].ToString());
+                }
+                while (i < numArray.Length)
+                {
+                    newnum = newnum + Math.Pow(numArray[i], 2);
+                    i++;
+                }
+                if (newnum == 1)
+                    Happy = true;
+                n = int.Parse(newnum.ToString());
+            }
+            return Happy;
+        }
+        public static int SingleNumber(int[] nums)
+        {
+            int Exit = 0, actual = 0;
+            if (nums.Length == 1)
+                return nums[0];
+            else
+            {
+                while (actual < nums.Length)
+                {
+                    int cont = 0;
+                    Exit = nums[actual];
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (nums[i] == Exit)
+                            cont++;
+                    }
+                    actual++;
+                    if (cont == 1)
+                        break;
+                }
+                return Exit;
+            }
+
+        }
+        //MEJORABLE 
+        public static bool UniqueOccurrences(int[] arr)
+        {
+            int Acumulator_A = 0, Acumilator_B = 0, Num_A = 0, Num_B = 1;
+
+            Array.Sort(arr);
+
+            while (Num_A < arr.Length)
+            {
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    Num_B = i;
+                    if (arr[Num_A] != arr[i])
+                    {
+                        Acumulator_A = numsOfRepet(arr, arr[Num_A]);
+                        Acumilator_B = numsOfRepet(arr, arr[Num_B]);
+                        if (Acumulator_A == Acumilator_B)
+                            return false;
+                    }
+                }
+                Num_A++;
+            }
+            return true;
+        }
+        public static int numsOfRepet(int[] arr, int num)
+        {
+            int Repet_Times = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                    Repet_Times++;
+            }
+            return Repet_Times;
+        }
+    }
+}
---
+++ testcamp/test.cs
@@ -0,0 +1,92 @@
+﻿using leetcodex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testcamp
+{
+    public class test
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(10, false)]
+        [InlineData(-121, false)]
+        [InlineData(2323, false)]
+        [InlineData(112211, true)]
+        [InlineData(332233, true)]
+        [InlineData(552155, false)]
+        public void NumberPal_IsPalindrome_ReturnBool(int NumberTest, bool Expected)
+        {
+            //TEST 
+            bool o = Solutions.IsPalindrome(NumberTest);
+            Assert.Equal(Expected, o);
+        }
+        //Este no tiene sentido
+        [Fact]
+        public void NumberPal_TwoSum_ReturnArray()
+        {
+            //TEST 1
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] resultado = Solutions.TwoSum(nums, target);
+            Assert.Equal(9, target);
+
+            //TEST 2
+            int[] nums2 = { 3, 3 };
+            int target2 = 6;
+            int[] resultado2 = Solutions.TwoSum(nums2, target2);
+            Assert.Equal(6, target2);
+        }
+        [Theory]
+        [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        [InlineData(new[] { 0, 1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 6, 6, 7, 8 }, 9)]
+        [InlineData(new[] { 0 }, 1)]
+        public void NumberPal_RemoveDuplicates_ReturnInt(int[] arrayTest, int Expected)
+        {
+            //TEST 
+            int result = Solutions.RemoveDuplicates(arrayTest);
+            Assert.Equal(Expected, result);
+        }
+        [Theory]
+        [InlineData(19, true)]
+        [InlineData(2, false)]
+        [InlineData(7, true)]
+        public void NumberPal_IsHappy_ReturnBool(int number, bool Expected)
+        {
+            //TEST 
+            bool result = Solutions.IsHappy(number);
+            Assert.Equal(Expected, result);
+        }
+        [Theory]
+        [InlineData(new[] { 2, 2, 1 }, 1)]
+        [InlineData(new[] { 4, 1, 2, 1, 2 }, 4)]
+        [InlineData(new[] { 4, 1, 2, 1, 4 }, 2)]
+        public void NumberPal_SingleNumber_ReturnInt(int[] arrayTest, int Expected)
+        {
+            //TEST 
+            int result = Solutions.SingleNumber(arrayTest);
+            Assert.Equal(Expected, result);
+        }
+        [Fact]
+        public void NumberPal_UniqueOccurrences_ReturnBool()
+        {
+            bool result;
+            //TEST 1
+            int[] a = { 26, 2, 16, 16, 5, 5, 26, 2, 5, 20, 20, 5, 2, 20, 2, 2, 20, 2, 16, 20, 16, 17, 16, 2, 16, 20, 26, 16 };
+            result = Solutions.UniqueOccurrences(a);
+            Assert.False(result);
+
+            //TEST 2
+            int[] b = { 1, 2 };
+            result = Solutions.UniqueOccurrences(b);
+            Assert.False(result);
+
+            //TEST 3
+            int[] c = { 1, 2, 2, 1, 1, 3 };
+            result = Solutions.UniqueOccurrences(c);
+            Assert.True(result);
+        }
+    }
+}
