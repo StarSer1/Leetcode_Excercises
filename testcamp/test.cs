c88c5ee20000f5358db4b1079c320e2dc5d955bc
StarSer <androidemoc@gmail.com>
2024-01-31 16:41:43 -0700

    DANGER

--- testcamp/test.cs
+++ testcamp/test.cs
@@ -74,5 +74,24 @@ namespace testcamp
             result = Solutions.UniqueOccurrences(c);
             Assert.True(result);
         }
+        [Fact]
+        public void Explosion()
+        {
+            bool result;
+            //TEST 1
+            int[] a = { 26, 2, 16, 16, 5, 5, 26, 2, 5, 20, 20, 5, 2, 20, 2, 2, 20, 2, 16, 20, 16, 17, 16, 2, 16, 20, 26, 16 };
+            result = Solutions.UniqueOccurrences(a);
+            Assert.False(result);
+
+            //TEST 2
+            int[] b = { 1, 2 };
+            result = Solutions.UniqueOccurrences(b);
+            Assert.False(result);
+
+            //TEST 3
+            int[] c = { 1, 2, 2, 1, 1, 3 };
+            result = Solutions.UniqueOccurrences(c);
+            Assert.True(result);
+        }
     }
 }
